9f69c971901298c671e734a14db02a8b42853ae0
Glayson B. Santos <glayson@MacBook-Pro-de-Glayson.local>
2024-11-08 04:09:34 -0300

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/senai-prod",
+  assetPrefix: "/senai-prod/",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
